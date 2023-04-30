f81358cccda0477bdf3db21582f0ef130f5bd7d7
seekersoftec <seekersoftec@gmail.com>
2023-04-30 21:32:39 +0100

    updated webservice server

---
+++ webservice/server/application/database/database.py
@@ -0,0 +1,10 @@
+
+class DataBaseSQLStore:
+    """
+        Store historical data, analysis and the current state of the (trading) environment including its history in Redis
+    """
+    def __init__(self) -> None:
+        pass
+
+    async def get_signal(self):
+        return
---
+++ webservice/server/application/database/redis_store.py
@@ -0,0 +1,7 @@
+
+class RedisStore:
+    """
+        Store historical data, analysis and the current state of the (trading) environment including its history in Redis
+    """
+    def __init__(self) -> None:
+        pass
